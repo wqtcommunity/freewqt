9414981af6264251fefceb30dd26e66025066592
A. Korhonen <akorhonen@pm.me>
2021-11-12 07:56:03 +0330

    Minor change

--- resources/views/pages/includes/_footer.blade.php
+++ resources/views/pages/includes/_footer.blade.php
@@ -1,5 +1,5 @@
 <div class="container-lg text-center">
     <section id="note" class="px-5 py-3 my-4">
-        Made with <span class="heart">&hearts;</span> by unofficial community developers, <a href="https://workquest.co/" target="_blank">WorkQuest</a> fans and supporters.
+        Made with <span class="heart">&hearts;</span> by <a href="https://workquest.co/" target="_blank">WorkQuest</a> fans and supporters.
     </section>
 </div>
