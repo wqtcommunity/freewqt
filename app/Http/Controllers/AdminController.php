f47e7303d9e9a6bc96a17a846fd126cec47b8367
A. Korhonen <akorhonen@pm.me>
2021-11-12 09:39:29 +0330

    Minor change

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -37,7 +37,7 @@ class AdminController extends Controller
 
     public function users()
     {
-        $users = User::orderBy('id','DESC')->paginate(100);
+        $users = User::orderBy('id','asc')->paginate(200);
 
         return view('admin.users', compact('users'));
     }
