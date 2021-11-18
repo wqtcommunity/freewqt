13752fcab83ccb002a26e5ef19049fcabc149530
A. Korhonen <akorhonen@pm.me>
2021-11-18 22:07:13 +0330

    Update tasks.blade.php

--- resources/views/dashboard/tasks.blade.php
+++ resources/views/dashboard/tasks.blade.php
@@ -1,6 +1,7 @@
 @extends('layouts.dashboard', ['page_title' => 'tasks'])
 
 @section('content')
+    <div class="alert alert-info">After completing the tasks, please allow 6-24 hours for the system to check and approve them automatically.</div>
     <table class="table mb-5">
         <thead>
             <tr>
